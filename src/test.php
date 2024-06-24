5631f9c8f727bfd67c2ea135bfbbcc7f5e28d196
愛甲啓介 <step.up.to.brighter03@gmail.com name = ksk-aiko>
2024-06-24 16:41:32 +0900

    Menu iteration process added.

--- src/test.php
+++ src/test.php
@@ -1,64 +1,74 @@
 <?php
 echo '読書ログサービスをご利用いただき、ありがとうございます'.PHP_EOL;
-echo '番号を選択してください(1,2,9)'.PHP_EOL;
-$num = trim(fgets(STDIN));
-# 各変数を初期化する
-$book_name = '';
-$author = '';
-$status = '';
-$score = '';
-$impression = '';
+while (true) {
 
-
-if ($num == '1') {
-    
-    echo'1.読書ログを登録します'.PHP_EOL;
-    echo PHP_EOL;
-    echo '書籍名を入力してください'.PHP_EOL;
-    $book_name = trim(fgets(STDIN));
-
-    echo '著者名を入力してください'.PHP_EOL;
-    $author = trim(fgets(STDIN));
-
-    echo '読書状況を入力してください'.PHP_EOL;
-    $status = trim(fgets(STDIN));
-
-    echo '評価を入力してください(1~5点)'.PHP_EOL;
-    $score = trim(fgets(STDIN));
-
-    echo '感想を入力してください'.PHP_EOL;
-    $impression = trim(fgets(STDIN));
-
+    echo '1.読書ログを登録'.PHP_EOL;
+    echo '2.読書ログを表示'.PHP_EOL;
+    echo '9.アプリケーションを終了'.PHP_EOL;
     echo PHP_EOL;
 
-    echo '登録が完了しました'.PHP_EOL;
-    echo '登録情報を表示します'.PHP_EOL;
+    echo '番号を選択してください(1,2,9)'.PHP_EOL;
+    $num = trim(fgets(STDIN));
+    # 各変数を初期化する
+    $book_name = '';
+    $author = '';
+    $status = '';
+    $score = '';
+    $impression = '';
 
-    echo PHP_EOL;
 
-    echo '----------------------'.PHP_EOL;
-    echo '書籍名：'.$book_name.PHP_EOL;
-    echo '著者名：'.$author.PHP_EOL;
-    echo '読書状況：'.$status.PHP_EOL;
-    echo '評価：'.$score.PHP_EOL;
-    echo '感想：'.$impression.PHP_EOL;
-    echo '----------------------'.PHP_EOL;
+    if ($num == '1') {
+        
+        echo'1.読書ログを登録します'.PHP_EOL;
+        echo PHP_EOL;
+        echo '書籍名を入力してください'.PHP_EOL;
+        $book_name = trim(fgets(STDIN));
 
-} elseif ($num == '2') {
-    echo '2.読書ログを表示します'.PHP_EOL;
-    echo  PHP_EOL;
-    echo '----------------------'.PHP_EOL;
-    echo '書籍名：'.$book_name.PHP_EOL;
-    echo '著者名：'.$author.PHP_EOL;
-    echo '読書状況：'.$status.PHP_EOL;
-    echo '評価：'.$score.PHP_EOL;
-    echo '感想：'.$impression.PHP_EOL;
-    echo '----------------------'.PHP_EOL;
-} elseif ($num == '9') {
-    echo '9.アプリケーションを終了します'.PHP_EOL;
-    return;
-} else {
-    echo '無効な値です'.PHP_EOL;
-    return;
-}
+        echo '著者名を入力してください'.PHP_EOL;
+        $author = trim(fgets(STDIN));
+
+        echo '読書状況を入力してください'.PHP_EOL;
+        $status = trim(fgets(STDIN));
+
+        echo '評価を入力してください(1~5点)'.PHP_EOL;
+        $score = trim(fgets(STDIN));
+
+        echo '感想を入力してください'.PHP_EOL;
+        $impression = trim(fgets(STDIN));
+
+        echo PHP_EOL;
+
+        echo '登録が完了しました'.PHP_EOL;
+        echo '登録情報を表示します'.PHP_EOL;
+
+        echo PHP_EOL;
+
+        echo '----------------------'.PHP_EOL;
+        echo '書籍名：'.$book_name.PHP_EOL;
+        echo '著者名：'.$author.PHP_EOL;
+        echo '読書状況：'.$status.PHP_EOL;
+        echo '評価：'.$score.PHP_EOL;
+        echo '感想：'.$impression.PHP_EOL;
+        echo '----------------------'.PHP_EOL;
+        echo PHP_EOL;
+
+    } elseif ($num == '2') {
+        echo '2.読書ログを表示します'.PHP_EOL;
+        echo  PHP_EOL;
+        echo '----------------------'.PHP_EOL;
+        echo '書籍名：'.$book_name.PHP_EOL;
+        echo '著者名：'.$author.PHP_EOL;
+        echo '読書状況：'.$status.PHP_EOL;
+        echo '評価：'.$score.PHP_EOL;
+        echo '感想：'.$impression.PHP_EOL;
+        echo '----------------------'.PHP_EOL;
+        echo PHP_EOL;
+    } elseif ($num == '9') {
+        echo '9.アプリケーションを終了します'.PHP_EOL;
+        return;
+    } else {
+        echo '無効な値です'.PHP_EOL;
+        return;
+    }
+    }
 
